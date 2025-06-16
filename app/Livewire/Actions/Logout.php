470e9c2f44b76e7105262796b554b8242408da3b
Patrick de Freitas <patrickfreitas.dev@gmail.com>
2025-06-16 09:43:15 +0800

    HEY-2: Installing Larastan and fixing errors

--- app/Livewire/Actions/Logout.php
+++ app/Livewire/Actions/Logout.php
@@ -3,13 +3,14 @@
 namespace App\Livewire\Actions;
 
 use Illuminate\Support\Facades\{Auth, Session};
+use Illuminate\Http\RedirectResponse;
 
 class Logout
 {
     /**
      * Log the current user out of the application.
      */
-    public function __invoke()
+    public function __invoke(): RedirectResponse
     {
         Auth::guard('web')->logout();
 
