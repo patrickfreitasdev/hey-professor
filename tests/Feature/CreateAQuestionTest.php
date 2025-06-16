297c12d312e0210a2af2b6f980757489322c6005
Patrick de Freitas <patrickfreitas.dev@gmail.com>
2025-06-16 14:30:46 +0800

    HEY-8: it should check if ends with a question mark

--- tests/Feature/CreateAQuestionTest.php
+++ tests/Feature/CreateAQuestionTest.php
@@ -1,29 +1,46 @@
 <?php
 
+use App\Models\User;
+
+use function Pest\Laravel\{actingAs, assertDatabaseCount, assertDatabaseHas, post};
+
 it("should be able to create a new question bigger than 10 characters", function () {
 
     //AAA
 
     // Arrange
-    $user = \App\Models\User::factory()->create();
-    \Pest\Laravel\actingAs($user);
+    $user = User::factory()->create();
+    actingAs($user);
 
     // Act
 
-    $request = \Pest\Laravel\post(route('question.store'), [
+    $request = post(route('question.store'), [
         'question' => str_repeat('*', 260) . '?',
     ]);
 
     // Assert
     $request->assertRedirect(route('dashboard'));
-    \Pest\Laravel\assertDatabaseCount('questions', 1);
-    \Pest\Laravel\assertDatabaseHas('questions', [
+    assertDatabaseCount('questions', 1);
+    assertDatabaseHas('questions', [
         'question' => str_repeat('*', 260) . '?',
     ]);
 
 });
 
 it("should check if ends with a question mark", function () {
+
+    // Arrange
+    $user = User::factory()->create();
+    actingAs($user);
+
+    // Act
+    $request = post(route('question.store'), [
+        'question' => str_repeat('*', 8) . '?',
+    ]);
+
+    // Assert
+    $request->assertSessionHasErrors(['question' => __('validation.min.string', ['min' => 10, 'attribute' => 'question'])]);
+    assertDatabaseCount('questions', 0);
 
 });
 
