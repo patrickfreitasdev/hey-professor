38d8c00c18f1eb076f66cea707a335456b6dbf73
Patrick de Freitas <patrickfreitas.dev@gmail.com>
2025-06-16 11:14:05 +0800

    HEY-2: Fixing return type in livewire

--- app/Livewire/Actions/Logout.php
+++ app/Livewire/Actions/Logout.php
@@ -2,15 +2,16 @@
 
 namespace App\Livewire\Actions;
 
-use Illuminate\Support\Facades\{Auth, Session};
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\{Auth, Session};
+use Livewire\Features\SupportRedirects\Redirector;
 
 class Logout
 {
     /**
      * Log the current user out of the application.
      */
-    public function __invoke(): RedirectResponse
+    public function __invoke(): RedirectResponse|Redirector
     {
         Auth::guard('web')->logout();
 
