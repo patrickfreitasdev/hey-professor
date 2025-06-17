2684a7c06e5431d7a17904ca59dac5de576bf583
Patrick de Freitas <patrickfreitas.dev@gmail.com>
2025-06-17 16:55:17 +0800

    HEY-8: Refactor front end to use blade template components

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,20 +1,9 @@
 <x-layouts.app :title="__('Dashboard')">
 
-    <form action="{{ route('question.store') }}" method="post" class="max-w-sm mx-auto">
-        @csrf
-        <div class="mb-4">
-            <label for="question" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ask me anything</label>
-            <textarea
-                    name="question" id="question" rows="4"
-                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500
-                    @error('question') bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500 @enderror
-                    " placeholder="Your question...">{{old('question')}}</textarea>
-            @error('question')
-            <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{$message}}</p>
-            @enderror
-        </div>
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 cursor-pointer dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Save</button>
-        <button type="reset" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 cursor-pointer">Cancel</button>
-    </form>
+    <x-form post :action="route('question.store')">
+        <x-textarea label="Question" name="question"/>
+        <x-btn.primary type="submit">Save</x-btn.primary>
+        <x-btn.reset type="reset">Cancel</x-btn.reset>
+    </x-form>
 
 </x-layouts.app>
