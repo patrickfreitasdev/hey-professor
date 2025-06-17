4953e261d43bcdbb10bae80181e3fec24272bad4
Patrick de Freitas <patrickfreitas.dev@gmail.com>
2025-06-17 19:30:51 +0800

    HEY-11: It Should not be able to like more than one time

--- app/Models/User.php
+++ app/Models/User.php
@@ -70,7 +70,7 @@ class User extends Authenticatable
     }
     public function like(Question $question): void
     {
-        $this->votes()->create([
+        $this->votes()->updateOrCreate([
             'question_id' => $question->id,
             'like'        => 1,
             'unlike'      => 0,
--- tests/Feature/VoteAQuestionTest.php
+++ tests/Feature/VoteAQuestionTest.php
@@ -24,3 +24,26 @@ it("Should be able to like a question", function () {
     ]);
 
 });
+
+it("Should not be able to like more than one time", function () {
+
+    //Arrange
+    $user = User::factory()->create();
+    actingAs($user);
+
+    $question = Question::factory()->create();
+
+    // Acting
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+
+    expect(
+        $user
+            ->votes()
+            ->where('question_id', '=', $question->id)->count()
+    )
+            ->toBe(1);
+
+});
