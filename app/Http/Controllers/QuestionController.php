9ee6100e19a5d8b0ded0eed981c6a43bf275a6e9
Patrick de Freitas <patrickfreitas.dev@gmail.com>
2025-07-07 21:00:06 +0800

    HEY-15: It should make sure that only question with status DRAFT can be edited

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -22,6 +22,8 @@ class QuestionController extends Controller
     public function edit(Question $question): View
     {
 
+        Gate::authorize('update', $question);
+
         return \view('question.edit', compact('question'));
 
     }
--- tests/Feature/Question/EditTest.php
+++ tests/Feature/Question/EditTest.php
@@ -27,3 +27,18 @@ it("Should return a view", function () {
     get(route('question.edit', $question))->assertViewIs('question.edit');
 
 });
+
+it("Should make sure that only question with status DRAFT can be edited", function () {
+
+    $user             = User::factory()->create();
+    $questionNotDraft = Question::factory()
+        ->for($user, 'createdBy')->create(['draft' => false]);
+    $draftQuestion = Question::factory()
+        ->for($user, 'createdBy')->create(['draft' => true]);
+
+    actingAs($user);
+
+    get(route('question.edit', $questionNotDraft))->assertForbidden();
+    get(route('question.edit', $draftQuestion))->assertSuccessful();
+
+});
