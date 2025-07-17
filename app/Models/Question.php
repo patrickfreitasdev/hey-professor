e92e93f925b5cfbe376cb7dfb49496eab1de7667
Patrick de Freitas <patrickfreitas.dev@gmail.com>
2025-07-17 20:32:16 +0800

    HEY-17: it Should prune records deleted more than one month

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -3,8 +3,8 @@
 namespace App\Models;
 
 use Database\Factories\QuestionFactory;
+use Illuminate\Database\Eloquent\{Builder, Model, Prunable, SoftDeletes};
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\{Model, SoftDeletes};
 use Illuminate\Database\Eloquent\Relations\{BelongsTo, HasMany};
 
 class Question extends Model
@@ -13,6 +13,7 @@ class Question extends Model
     use HasFactory;
 
     use SoftDeletes;
+    use Prunable;
 
     protected $casts = [
         'draft' => 'bool',
@@ -29,5 +30,15 @@ class Question extends Model
     public function createdBy(): BelongsTo
     {
         return $this->belongsTo(User::class, 'created_by');
+    }
+
+    /**
+     * Get the prunable model query.
+     *
+     * @return Builder<Question>
+     */
+    public function prunable(): Builder
+    {
+        return static::where('deleted_at', '<=', now()->subMonth());
     }
 }
